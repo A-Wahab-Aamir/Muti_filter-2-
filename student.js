dada1ab3442d58860c10a13beda128e3541f9a7a
Abdul Wahab Amir <83786802+A-Wahab-Aamir@users.noreply.github.com>
2023-12-08 16:23:15 +0500

    updated_multi_filter

---
+++ student.js
@@ -0,0 +1,72 @@
+var data=[
+    {
+        id:"0",
+        "Name":"Ahmed Ali",
+        "f_Name":"Yaqoob Ali",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s1.jpg",
+        "gender":"male",
+        "dept":"engineering"
+    },
+    {
+        id:"1",
+        "Name":"Yasir",
+        "f_Name":"Mahmood",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s2.jpg",
+        "gender":"male",
+        "dept":"Bio"
+    },
+    {
+        id:"2",
+        "Name":"Alia Khan",
+        "f_Name":"Tameez khan",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s3.jpg",
+        "gender":"Female",
+        "dept":"engineering"
+    },
+    {
+        id:"3",
+        "Name":"Maria Khan",
+        "f_Name":"Bilal Khan",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s4.jpg",
+        "gender":"Female",
+        "dept":"Bio"
+    },
+    {
+        id:"4",
+        "Name":"Farooq Hadiyat",
+        "f_Name":"Hadiayat",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s5.jpg",
+        "gender":"Male",
+        "dept":"engineering"
+    },
+    {
+        id:"5",
+        "Name":"Maroof Ali",
+        "f_Name":"khan Ali",
+        "class":"VB",
+        "contact":"0311-823674",
+        "profile":"student_images/s5.jpg",
+        "gender":"Male",
+        "dept":"engineering"
+    },
+    {
+        id:"6",
+        "Name":"Khan zada",
+        "f_Name":"khan Ali",
+        "class":"ViB",
+        "contact":"0311-823674",
+        "profile":"student_images/s2.jpg",
+        "gender":"male",
+        "dept":"Bio"
+    },
+]
